3e0943a2576efdab699a61b31486b4cedd78dd22
中田誠人 <masat@MacBook-Pro-3.local>
2021-12-03 23:59:43 +0900

    機能完了

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-   @words = Word.where(user_id: current_user.id) 
+    if user_signed_in?
+      @words = Word.where(user_id: current_user.id) 
+    end
   end
 end
